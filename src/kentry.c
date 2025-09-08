ba2fe243fefd65538c00d99f4ca834152edfc944
vmwarehater <nearestcommit@gmail.com>
2025-09-08 15:38:40 -0500

    nearvm can now execute more then one line, and it now can store funcs and jump to it

--- src/kentry.c
+++ src/kentry.c
@@ -26,38 +26,65 @@ void kernel_entry(void){
     load_exception_vector();
     puts("Loaded Core Utilites.....\n\n");
 
-    // mov r0, 0 (sets kcall so it prints the value in r1)
-    uint64_t ins1[3];
-    ins1[0] = 0x2;
-    ins1[1] = 0x0;
-    ins1[2] = 0x0;
-    interpret_vm_bytecode_line(ins1);
+    // // mov r0, 0 (sets kcall so it prints the value in r1)
+    // uint64_t ins1[3];
+    // ins1[0] = 0x2;
+    // ins1[1] = 0x0;
+    // ins1[2] = 0x0;
+    // interpret_vm_bytecode_line(ins1);
 
-    // mov r2, 3
-    ins1[0] = 0x2;
-    ins1[1] = 0x2;
-    ins1[2] = 0x3;
-    interpret_vm_bytecode_line(ins1);
+    // // mov r2, 3
+    // ins1[0] = 0x2;
+    // ins1[1] = 0x2;
+    // ins1[2] = 0x3;
+    // interpret_vm_bytecode_line(ins1);
 
-    // mov r3, 3
-    ins1[0] = 0x2;
-    ins1[1] = 0x3;
-    ins1[2] = 0x2;
-    interpret_vm_bytecode_line(ins1);
+    // // mov r3, 3
+    // ins1[0] = 0x2;
+    // ins1[1] = 0x3;
+    // ins1[2] = 0x2;
+    // interpret_vm_bytecode_line(ins1);
 
-    // add r1, r2, r3
-    uint64_t ins2[4];
-    ins2[0] = 0x8;
-    ins2[1] = 0x1;
-    ins2[2] = 0x2;
-    ins2[3] = 0x3;
-    interpret_vm_bytecode_line(ins2);
+    // // add r1, r2, r3
+    // uint64_t ins2[4];
+    // ins2[0] = 0x8;
+    // ins2[1] = 0x1;
+    // ins2[2] = 0x2;
+    // ins2[3] = 0x3;
+    // interpret_vm_bytecode_line(ins2);
 
-    // kcall (prints value in r1)
-    uint64_t ins3[1];
-    ins3[0] = 0x6;
-    interpret_vm_bytecode_line(ins3);
+    // // kcall (prints value in r1)
+    // uint64_t ins3[1];
+    // ins3[0] = 0x6;
+    // interpret_vm_bytecode_line(ins3);
     
+    uint64_t ins[20];
+    // mov r0, 0 (kcall checks for r0, 0 means print whatever is in r1)
+    ins[0] = 0x2;
+    ins[1] = 0x0;
+    ins[2] = 0x0;
+    // mov r2, 3
+    ins[3] = 0x2;
+    ins[4] = 0x2;
+    ins[5] = 0x3;
+    // mov r3, 2
+    ins[6] = 0x2;
+    ins[7] = 0x3;
+    ins[8] = 0x2;
+    // sub r1, r2, r3
+    ins[9] = 0x8;
+    ins[10] = 0x1;
+    ins[11] = 0x2;
+    ins[12] = 0x3;
+    // func 0x1 
+    ins[13] = 0x9;
+    ins[14] = 0x1;
+    // kcall
+    ins[15] = 0x6;
+    // jmp 0x1
+    ins[16] = 0x5;
+    ins[17] = 0x1;
+    interpret_vm_bytecode(ins);
     
     puts("\n\nKernel finished and no init found, starting kernel debugger.....");
     // system ends, start emergency shell on the UART
--- src/system/nearvm/nearvm.c
+++ src/system/nearvm/nearvm.c
@@ -1,13 +1,18 @@
 #include "nearvm.h"
 #include <stdint.h>
 #include "../klibc/puts.h"
-
+#include "../mem/chunkalloc.h"
 
 
 
 uint64_t genreg[30] = {0};
+uint64_t specreg[10] = {0};
 
 
+typedef struct _func {
+    uint64_t key;
+    uint64_t curbycode;
+} func_t;
 enum instructions {
     ADD = 0x0,
     SUB = 0x1,
@@ -17,56 +22,76 @@ enum instructions {
     JMP = 0x5,
     KCALL = 0x6,
     MUL = 0x7,
-    DIV = 0x8
+    DIV = 0x8,
+    FUNC = 0x9
 };
 
+enum SPECIAL_REGISTERS {
+    FUNC_AM = 0
+};
 
-static inline void add_ins(uint64_t* bytecode){
-    if(bytecode[1] >= 30 || bytecode[2] >= 30
-        || bytecode[3] >= 30) return;
-    genreg[bytecode[1]] = genreg[bytecode[2]] + genreg[bytecode[3]];
+static inline void add_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] >= 30 || bytecode[cur_reg + 2] >= 30
+        || bytecode[cur_reg + 3] >= 30) return;
+    genreg[bytecode[cur_reg + 1]] = genreg[bytecode[cur_reg + 2]] + genreg[bytecode[cur_reg + 3]];
 }
 
-static inline void sub_ins(uint64_t* bytecode){
-    if(bytecode[1] >= 30 || bytecode[2] >= 30
-        || bytecode[3] >= 30) return;
-    genreg[bytecode[1]] = genreg[bytecode[2]] - genreg[bytecode[3]];
+static inline void sub_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] >= 30 || bytecode[cur_reg + 2] >= 30
+        || bytecode[cur_reg + 3] >= 30) return;
+    genreg[bytecode[cur_reg + 1]] = genreg[bytecode[cur_reg + 2]] - genreg[bytecode[cur_reg + 3]];
 }
 
-static inline void mul_ins(uint64_t* bytecode){
-    if(bytecode[1] >= 30 || bytecode[2] >= 30
-        || bytecode[3] >= 30) return;
-    genreg[bytecode[1]] = genreg[bytecode[2]] * genreg[bytecode[3]];
+static inline void mul_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] >= 30 || bytecode[cur_reg + 2] >= 30
+        || bytecode[cur_reg + 3] >= 30) return;
+    genreg[bytecode[cur_reg + 1]] = genreg[bytecode[cur_reg + 2]] * genreg[bytecode[cur_reg + 3]];
 }
 
-static inline void div_ins(uint64_t* bytecode){
-    if(bytecode[1] >= 30 || bytecode[2] >= 30
-        || bytecode[3] >= 30) return;
-    genreg[bytecode[1]] = genreg[bytecode[2]] / genreg[bytecode[3]];
+static inline void div_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] >= 30 || bytecode[cur_reg + 2] >= 30
+        || bytecode[cur_reg + 3] >= 30) return;
+    genreg[bytecode[cur_reg + 1]] = genreg[bytecode[cur_reg + 2]] / genreg[bytecode[cur_reg + 3]];
 }
 
 
-static inline void mov_ins(uint64_t* bytecode){
-    if(bytecode[1] >= 30 || bytecode[2] >= 30) return;
-    genreg[bytecode[1]] = bytecode[2];
+static inline void mov_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] >= 30 || bytecode[cur_reg + 2] >= 30) return;
+    genreg[bytecode[cur_reg + 1]] = bytecode[cur_reg + 2];
 }
 
-static inline void load_ins(uint64_t* bytecode){
-    if(bytecode[1] == (uint64_t)NULL || bytecode[2] == (uint64_t)NULL) return;
-
-}
-
-static inline void store_ins(uint64_t* bytecode){
-    if(bytecode[1] == (uint64_t)NULL || bytecode[2] == (uint64_t)NULL) return;
-
-}
-
-static inline void jump_ins(uint64_t* bytecode){
-    if(bytecode[1] == (uint64_t)NULL || bytecode[2] == (uint64_t)NULL) return;
+static inline void load_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] == (uint64_t)NULL || bytecode[cur_reg + 2] == (uint64_t)NULL) return;
 
 }
 
-static inline void kernel_call_ins(uint64_t* bytecode){
+static inline void store_ins(uint64_t* bytecode, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] == (uint64_t)NULL || bytecode[cur_reg + 2] == (uint64_t)NULL) return;
+
+}
+
+static inline void jump_ins(uint64_t* bytecode, func_t* stack, uint64_t* cur_reg){
+    uint64_t val = *cur_reg;
+    if(bytecode[val + 1] == (uint64_t)NULL) return;
+    for(uint64_t i = 0; i >= specreg[FUNC_AM]; i++){
+        if(stack[i].key == bytecode[val + 1]){
+            *cur_reg = stack[i].curbycode;
+            return;
+        }
+    }
+}
+
+static inline void func_ins(uint64_t* bytecode, func_t* stack, uint64_t cur_reg){
+    if(bytecode[cur_reg + 1] == (uint64_t)NULL) return;
+    if(specreg[FUNC_AM] * sizeof(uint64_t) >= 0x1000){
+        return;
+    }
+    puts("called again");
+    stack[specreg[FUNC_AM]].key = bytecode[cur_reg + 1];
+    stack[specreg[FUNC_AM]].curbycode = cur_reg;
+}
+
+static inline void kernel_call_ins(){
     switch (genreg[0]) {
         case 0:
             xputs(genreg[1]);
@@ -82,72 +107,84 @@ static inline void kernel_call_ins(uint64_t* bytecode){
 }
 
 
+void interpret_vm_bytecode(uint64_t* bytecode){
+    uint8_t run = 1;
+    uint64_t curbycode = 0;
+    func_t* stack = allocate_multiple_chunks(4); // 4Kbs
+    while(run == 1){
+        if(!bytecode[curbycode]) return;
+        switch(bytecode[curbycode]){
+            case ADD:
+                add_ins(bytecode, curbycode);
+                curbycode += 4;
+                break;
+            case SUB:
+                sub_ins(bytecode, curbycode);
+                curbycode += 4;
+                break;
+            case MOV:
+                mov_ins(bytecode, curbycode);
+                curbycode += 3;
+                break;
+            case LD:
+                load_ins(bytecode, curbycode);
+                break;
+            case STR:
+                store_ins(bytecode, curbycode);
+                break;
+            case JMP:
+                jump_ins(bytecode, stack, &curbycode);
+                curbycode += 2;
+                break;
+            case KCALL:
+                kernel_call_ins();
+                curbycode += 1;
+                break;
+            case MUL:
+                mul_ins(bytecode, curbycode);
+                curbycode += 4;
+                break;
+            case DIV:
+                div_ins(bytecode, curbycode);
+                curbycode += 4;
+                break;
+            case FUNC:
+                func_ins(bytecode, stack, curbycode);
+                curbycode += 2;
+                break;
+            default:
+                break;
+        }
+    }
+    free_multiple_chunks(stack, 4);
+    
+}
+
 void interpret_vm_bytecode_line(uint64_t* bytecode){
     switch(bytecode[0]){
         case ADD:
-            add_ins(bytecode);
+            add_ins(bytecode, 0);
             break;
         case SUB:
-            sub_ins(bytecode);
+            sub_ins(bytecode, 0);
             break;
         case MOV:
-            mov_ins(bytecode);
+            mov_ins(bytecode, 0);
             break;
         case LD:
-            load_ins(bytecode);
+            load_ins(bytecode, 0);
             break;
         case STR:
-            store_ins(bytecode);
-            break;
-        case JMP:
-            jump_ins(bytecode);
+            store_ins(bytecode, 0);
             break;
         case KCALL:
-            kernel_call_ins(bytecode);
+            kernel_call_ins();
             break;
         case MUL:
-            mul_ins(bytecode);
+            mul_ins(bytecode, 0);
             break;
         case DIV:
-            div_ins(bytecode);
-            break;
-        default:
-            // in here call INVALID_BYTECODE signal to the process which called it
-            // we don't have that infra yet so its a note for when we do implement it
-            break;
-    }
-}
-
-// multi-line bytecode interperter
-// TODO, implement this **correctly**
-void interpret_vm_bytecode(uint64_t* bytecode){
-    switch(bytecode[0]){
-        case ADD:
-            add_ins(bytecode);
-            break;
-        case SUB:
-            sub_ins(bytecode);
-            break;
-        case MOV:
-            mov_ins(bytecode);
-            break;
-        case LD:
-            load_ins(bytecode);
-            break;
-        case STR:
-            store_ins(bytecode);
-            break;
-        case JMP:
-            jump_ins(bytecode);
-            break;
-        case KCALL:
-            kernel_call_ins(bytecode);
-            break;
-        case MUL:
-            mul_ins(bytecode);
-            break;
-        case DIV:
-            div_ins(bytecode);
+            div_ins(bytecode, 0);
             break;
         default:
             // in here call INVALID_BYTECODE signal to the process which called it
